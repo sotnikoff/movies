b518084060299c57fba6fa65df49c469abd78764
Unknown <thehofch@gmail.com>
2018-06-27 09:29:04 +0300

    add movie cover image to main screen

--- app/javascript/movies-app/movies.vue
+++ app/javascript/movies-app/movies.vue
@@ -3,13 +3,19 @@
     <h1>Кинотеатр "Зачётная сосиска"</h1>
     <h4>Фильмы</h4>
     <div v-for="movie in movies">
-      <p><b><router-link :to="'/movies/' + movie.id">{{ movie.title }}</router-link></b></p>
-      <p>
-        {{ movie.description }}
-      </p>
-      <p>
-        {{ movie.adult_rating }}
-      </p>
+      <div class="movie">
+        <h1><b><router-link :to="'/movies/' + movie.id">{{ movie.title }}</router-link></b></h1>
+        <p>
+          <img :src="movie.image.cover.url">
+        </p>
+        <p>
+          {{ movie.description }}
+        </p>
+        <p>
+          {{ movie.adult_rating }}
+        </p>
+      </div>
+
     </div>
   </div>
 </template>
